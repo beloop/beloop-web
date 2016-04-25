3ba37346f0fee741e618bbb6e6497aa9b893d0a9
Arkaitz Garro <arkaitz.garro@gmail.com>
2016-04-25 20:37:18 +0200

    Dashboard bundle

---
+++ src/Beloop/Web/DashboardBundle/Controller/DashboardController.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * This file is part of the Beloop package.
+ *
+ * Copyright (c) 2016 AHDO Studio B.V.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ *
+ * @author Arkaitz Garro <arkaitz.garro@gmail.com>
+ */
+
+namespace Beloop\Web\DashboardBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DashboardController extends Controller
+{
+    /**
+     * Dashboard page
+     *
+     * @return array
+     *
+     * @Route(
+     *      path = "/dashboard",
+     *      name = "beloop_dashboard",
+     *      methods = {"GET"}
+     * )
+     *
+     * @Template
+     */
+    public function dashboardAction()
+    {
+        $user = $this->getUser();
+
+        return [
+            'section' => 'dashboard',
+            'user' => $user,
+        ];
+    }
+}
