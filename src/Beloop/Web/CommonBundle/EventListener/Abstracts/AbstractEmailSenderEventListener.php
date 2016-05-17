76a094bc420ae964307e3575bdf2e4506ccfe0aa
Arkaitz Garro <arkaitz.garro@gmail.com>
2016-05-17 21:05:56 +0200

    Send emails

--- src/Beloop/Web/CommonBundle/EventListener/Abstracts/AbstractEmailSenderEventListener.php
+++ src/Beloop/Web/CommonBundle/EventListener/Abstracts/AbstractEmailSenderEventListener.php
@@ -61,9 +61,6 @@ abstract class AbstractEmailSenderEventListener
             ->twig
             ->render($template, $context);
 
-        echo $body;
-        die('');
-
         $message = $this
             ->mailer
             ->createMessage()
