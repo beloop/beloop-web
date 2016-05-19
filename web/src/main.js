3a28c65474a7baee8bd99870b419d2562cd33307
Arkaitz Garro <arkaitz.garro@gmail.com>
2016-05-19 23:54:07 +0200

    Load spinner resources

--- web/src/main.js
+++ web/src/main.js
@@ -10,26 +10,27 @@ var App = (function () {
         openSidebarDelay: 400
     };
 
-    var body = $("body");
-    var wrapper = $(".am-wrapper");
-    var leftSidebar = $(".am-left-sidebar");
+    var body = $('body');
+    var wrapper = $('.am-wrapper');
+    var leftSidebar = $('.am-left-sidebar');
     var openSidebar = false;
+    var spinner, loader;
 
     //Core private functions
-    function leftSidebarInit() {
+    var leftSidebarInit = function leftSidebarInit() {
 
-        function oSidebar() {
-            body.addClass(config.openLeftSidebarClass + " " + config.transitionClass);
+        var oSidebar = function oSidebar() {
+            body.addClass(config.openLeftSidebarClass + ' ' + config.transitionClass);
             openSidebar = true;
-        }
+        };
 
-        function cSidebar() {
+        var cSidebar = function cSidebar() {
             body.removeClass(config.openLeftSidebarClass).addClass(config.transitionClass);
             sidebarDelay();
-        }
+        };
 
         /*Open-Sidebar when click on topbar button*/
-        $('.am-toggle-left-sidebar').on("click", function (e) {
+        $('.am-toggle-left-sidebar').on('click', function(e) {
             e.stopPropagation();
             e.preventDefault();
 
@@ -42,29 +43,54 @@ var App = (function () {
         });
 
         /*Close sidebar on click outside*/
-        $(document).on("touchstart mousedown", function (e) {
+        $(document).on('touchstart mousedown', function(e) {
             if (!$(e.target).closest(leftSidebar).length && body.hasClass(config.openLeftSidebarClass)) {
                 cSidebar();
             }
         });
 
         /*Calculate sidebar tree active & open classes*/
-        $("li.active", leftSidebar).parents(".parent").addClass("active open");
-    }
+        $('li.active', leftSidebar).parents('.parent').addClass('active open');
+    };
 
-    function sidebarDelay() {
+    var sidebarDelay = function sidebarDelay() {
         openSidebar = true;
         setTimeout(function () {
             openSidebar = false;
         }, config.openSidebarDelay);
-    }
+    };
+
+    var loaderInit = function loaderInit () {
+        var opts = {
+            lines: 16, // The number of lines to draw
+            length: 3, // The length of each line
+            width: 3, // The line thickness
+            radius: 14, // The radius of the inner circle
+            color: '#58806D', // #rgb or #rrggbb
+            speed: 2.1, // Rounds per second
+            trail: 60, // Afterglow percentage
+            shadow: false, // Whether to render a shadow
+            hwaccel: false // Whether to use hardware acceleration
+        };
+
+        loader = document.getElementById('loader');
+        spinner = new Spinner(opts).spin(document.getElementById('spin'));
+    };
+
+    var hideSpinner = function() {
+        spinner.stop();
+
+        if (loader) {
+            loader.classList.add('hidden');
+        }
+    };
 
     return {
         //General data
         conf: config,
 
         //Init function
-        init: function (options) {
+        init: function(options) {
 
             //Extends basic config with options
             $.extend(config, options);
@@ -72,11 +98,16 @@ var App = (function () {
             /*Left Sidebar*/
             leftSidebarInit();
 
+            /*Loader init*/
+            loaderInit();
+
             /*Body transition effect*/
-            leftSidebar.on("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function () {
+            leftSidebar.on('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd', function () {
                 body.removeClass(config.transitionClass);
             });
-        }
+        },
+
+        hideSpinner: hideSpinner
     };
 
 })();
