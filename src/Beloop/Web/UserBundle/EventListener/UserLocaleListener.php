42acd231a45de39bf7c8727536f2df622bbfa480
Arkaitz Garro <arkaitz.garro@gmail.com>
2016-05-25 21:07:19 +0200

    Translate web messages to Spanish.

---
+++ src/Beloop/Web/UserBundle/EventListener/UserLocaleListener.php
@@ -0,0 +1,78 @@
+<?php
+
+/*
+ * This file is part of the Beloop package.
+ *
+ * Copyright (c) 2016 AHDO Studio B.V.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ *
+ * @author Arkaitz Garro <arkaitz.garro@gmail.com>
+ */
+
+namespace Beloop\Web\UserBundle\EventListener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
+
+/**
+ * Stores the locale of the user in the session after the
+ * login. This can be used by the LocaleListener afterwards.
+ */
+class UserLocaleListener implements EventSubscriberInterface
+{
+    /**
+     * @var Session
+     */
+    private $session;
+
+    private $defaultLocale;
+
+    public function __construct(Session $session, $defaultLocale = 'en')
+    {
+        $this->session = $session;
+        $this->defaultLocale = $defaultLocale;
+    }
+
+    /**
+     * @param InteractiveLoginEvent $event
+     */
+    public function onInteractiveLogin(InteractiveLoginEvent $event)
+    {
+        $user = $event->getAuthenticationToken()->getUser();
+
+        if (null !== $user->getLanguage()) {
+            $this->session->set('_locale', $user->getLanguage()->getIso());
+        }
+    }
+
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        $request = $event->getRequest();
+        if (!$request->hasPreviousSession()) {
+            return;
+        }
+
+        // try to see if the locale has been set as a _locale routing parameter
+        if ($locale = $request->attributes->get('_locale')) {
+            $request->getSession()->set('_locale', $locale);
+        } else {
+            // if no explicit locale has been set on this request, use one from the session
+            $request->setLocale($request->getSession()->get('_locale', $this->defaultLocale));
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            // must be registered after the default Locale listener
+            KernelEvents::REQUEST => array(array('onKernelRequest', 15)),
+        );
+    }
+}
