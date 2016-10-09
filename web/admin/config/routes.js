03526b012152d0fd86017bbc6b858dc1419a93fe
Arkaitz Garro <arkaitz.garro@gmail.com>
2016-10-09 16:41:42 +0200

    Admin translations API

--- web/admin/config/routes.js
+++ web/admin/config/routes.js
@@ -1,22 +1,31 @@
 angular.module('beloop.admin')
-// @ngInject
-.config(($stateProvider, $urlRouterProvider) => {
+    // @ngInject
+    .config(($locationProvider, $stateProvider, $urlRouterProvider) => {
+        // $locationProvider.html5Mode(true);
 
-    // Basic abstract states
-    $stateProvider
-        .state('root', {
-            abstract: true,
-            template: '<ui-view></ui-view>'
+        // Basic abstract states
+        $stateProvider
+            .state('root', {
+                abstract: true,
+                template: '<ui-view></ui-view>'
+            });
+
+        $stateProvider
+            .state('courses', {
+                url: '/courses',
+                parent: 'root',
+                template: require('../features/courses/list.view.html'),
+                controllerAs: 'ctrl',
+                controller: 'CoursesController'
+            });
+
+        $urlRouterProvider.otherwise(() => '/courses');
+    })
+    // @ngInject
+    .config(($translateProvider) => {
+        $translateProvider.useStaticFilesLoader({
+            prefix: '/admin/translations/',
+            suffix: '.json'
         });
-
-    $stateProvider
-        .state('courses', {
-            url: '/courses',
-            parent: 'root',
-            template: require('../features/courses/list.view.html'),
-            controllerAs: 'ctrl',
-            controller: 'CoursesController'
-        });
-
-    $urlRouterProvider.otherwise(() => '/courses');
-});
+        $translateProvider.preferredLanguage('en');
+    });
--- web/admin/module.js
+++ web/admin/module.js
@@ -1,5 +1,6 @@
 angular.module('beloop.admin', [
     'ui.router',
+    'pascalprecht.translate',
 
     'beloop.admin.config',
     'beloop.admin.directives',
