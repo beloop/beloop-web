018cc186dc575bd828f8d09ee6a5034c7dfc85d6
Arkaitz Garro <arkaitz.garro@gmail.com>
2020-03-17 19:53:36 +0100

    Remove reference to bootstrap.php.cache file

--- web/app.php
+++ web/app.php
@@ -3,7 +3,6 @@
 use Symfony\Component\HttpFoundation\Request;
 
 $loader = require dirname(__DIR__).'/app/config/bootstrap.php';
-include_once __DIR__.'/../var/bootstrap.php.cache';
 
 // Enable APC for autoloading to improve performance.
 // You should change the ApcClassLoader first argument to a unique prefix
